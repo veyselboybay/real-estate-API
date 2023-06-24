1d2d0cc1997cf7435869bbbb3bf927fe1dbd77a6
veyselboybay <veysel.boybay@outlook.com>
2023-06-24 14:21:35 -0400

    filtering data by query strings are ok

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -2,10 +2,20 @@ const listingModel = require('../models/listing')
 const mockData = require('../listings-data')
 
 const getAll = async (req, res) => {
-    return res.status(200).json(mockData)
+    const { city, transactionType, propertyType, beds } = req.query;
+    if (Object.keys(req.query).length === 0) {
+        return res.status(200).json({success:true,msg:'FULLFILLED',data:mockData})
+    }
+    try {
+        const filteredData = await mockData.filter(item => item.city === city && item.transactionType === transactionType && item.propertyType === propertyType && item.beds === beds);
+        return res.status(200).json({success:true,msg:'FULLFILLED',data:filteredData})
+    } catch (error) {
+        return res.status(400).json({success:false,msg:'REJECTED => '+error,data:filteredData})
+    }
 }
 const createListing = async (req, res) => {
     // return res.json(mockData);
+
 }
 
 module.exports = { getAll}
